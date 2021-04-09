3059df101b0b33c5f650a9b25d5b8f8c67cbaa76
yuanhao <guyuanhao@foxmail.com>
2021-04-09 23:55:34 +0800

    dev

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -1,16 +1,23 @@
 package leetcode;
 
 import leetcode.codilityTest.CoinSequence;
+import leetcode.codilityTest.Gardener;
 import leetcode.codilityTest.MaximumNumberAdder;
 
 public class Main {
 	public static void main(String... s) {
 
-		MaximumNumberAdder maximumNumberAdder = new MaximumNumberAdder();
+		Gardener gardener = new Gardener();
+//		int[] treeHeights = new int[]{3,4,5,3,7};
+//		int[] treeHeights = new int[]{1,2,3,4};
+		int[] treeHeights = new int[]{1,3,1,2};
+		System.out.println(gardener.solution(treeHeights));
+
+/*		MaximumNumberAdder maximumNumberAdder = new MaximumNumberAdder();
 		int initialNumber1 = -438;
 		int initialNumber2 = 638;
 		System.out.println(maximumNumberAdder.solution(initialNumber1));
-		System.out.println(maximumNumberAdder.solution(initialNumber2));
+		System.out.println(maximumNumberAdder.solution(initialNumber2));*/
 
 /*		CoinSequence coinSequence = new CoinSequence();
 		int[] coins = new int[]{1,1,0,1,1};
--- src/leetcode/codilityTest/Gardener.java
+++ src/leetcode/codilityTest/Gardener.java
@@ -3,15 +3,54 @@ package leetcode.codilityTest;
 public class Gardener {
     public int solution(int[] treesHeight) {
         int solution = 0;
+        boolean alreadyAsthetic = true;
+        int flag = 0;
+        for (int index=0; index<treesHeight.length; index++) {
+            if (alreadyAsthetic
+                    && index<treesHeight.length - 2
+                    && !checkIfAsthetic(treesHeight[index], treesHeight[index+1], treesHeight[index+2])) {
+                alreadyAsthetic = false;
+                flag = index;
+                continue;
+            }
+            if (!alreadyAsthetic
+                    && index<treesHeight.length - 2
+                    && !checkIfAsthetic(treesHeight[index], treesHeight[index+1], treesHeight[index+2])) {
+                return -1;
+            }
+            if (alreadyAsthetic) {
+                return 0;
+            }
+        }
+
         for(int index=0; index<treesHeight.length; index++) {
+            if (!(index>=flag && index<flag + 3)) {
+                continue;
+            }
             if (index == 0) {
                 //查看[(index+1), (index+2)]和[(index+2), (index+3)]之间的关系
                 if (checkIfAsthetic(treesHeight[index+1], treesHeight[index+2], treesHeight[index+3])) {
                     solution++;
                 }
+            } else if (index == treesHeight.length - 2) {
+                //查看[(index-2), (index-1)]和[(index-1), (index+1)]之间的关系
+                if (checkIfAsthetic(treesHeight[index - 2], treesHeight[index - 1], treesHeight[index + 1])) {
+                    solution++;
+                }
+            } else if (index == treesHeight.length - 1) {
+                //查看[(index-3), (index-2)]和[(index-2), (index-1)]之间的关系
+                if (checkIfAsthetic(treesHeight[index - 3], treesHeight[index - 2], treesHeight[index - 1])) {
+                    solution++;
+                }
+            } else {
+                //查看[(index-1), (index+1)]和[(index+1), (index+2)]之间的关系
+                if (checkIfAsthetic(treesHeight[index - 1], treesHeight[index + 1], treesHeight[index + 2])) {
+                    solution++;
+                }
             }
         }
-        return 0;
+
+        return solution;
     }
 
     private boolean checkIfAsthetic(int first, int second, int third) {
