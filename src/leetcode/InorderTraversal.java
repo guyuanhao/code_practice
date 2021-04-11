5a3f8f93b261efa5ca17a6b40a2362e62e8950d6
guyuanhao <guyuanhao@foxmail.com>
2021-04-12 00:03:30 +0800

    inorderTraversal

---
+++ src/leetcode/InorderTraversal.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.*;
+
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.empty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+
+        return result;
+    }
+}
